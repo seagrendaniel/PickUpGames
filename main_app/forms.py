58ef32b921ab3e621274fc9bf35c937d8bc6c7c9
blackdanamite <seagrendaniel@gmail.com>
2019-09-16 13:47:05 -0700

    Update user & profile model fix routing from signup

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,18 +1,28 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+# from .models import Profile
 
+
+positions = [
+    ('point guard', 'Point Guard'),
+    ('shooting guard', 'Shooting Guard'),
+    ('small forward', 'Small Forward'),
+    ('power forward', 'Power Forward'),
+    ('center', 'Center')
+]
 
 class SignupForm(UserCreationForm):
     first_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput (attrs={'placeholder':'Kobe'}))
     last_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput (attrs={'placeholder':'Bryant'}))
-    # position = forms.ChoiceField('Point Guard', 'Shooting Guard', 'Small Forward', 'Power Forward', 'Center', required=True, widget=forms.TextInput (attrs={'placeholder':'Shooting Guard'}))
+    position = forms.CharField(label='Position Played', required=True, widget=forms.Select (choices=positions))
+    height = forms.CharField(max_length=10, required=True, widget=forms.TextInput (attrs={'placeholder':'6\'6'}))
     location = forms.CharField(max_length=50, required=True, widget=forms.TextInput (attrs={'placeholder':'Los Angeles'}))
     homecourt = forms.CharField(max_length=50, required=False, widget=forms.TextInput (attrs={'placeholder':'Staples Center'}))
 
     
-    
+
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'location', 'homecourt', 'password1', 'password2']
+        fields = ('username', 'first_name', 'last_name', 'position', 'height', 'location', 'homecourt', 'password1', 'password2')
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup', views.signup, name='signup'),
+    path('profile/', views.profile_show, name='profile'),
 ]
