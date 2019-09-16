c1d4d7752faf1baccbd0a89852b7501b34632415
Nick Bai <nick@nick.com>
2019-09-16 16:58:33 -0700

    fix signup functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .forms import SignupForm
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,6 +11,19 @@ class Profile(models.Model):
     height = models.CharField(max_length=10)
     location = models.CharField(max_length=50)
     homecourt = models.CharField(max_length=50)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    
+    if created:
+        Profile.objects.create(user=instance)
+        
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    # instance.profile.height = SignupForm.height
+    # print(instance.profile.height)
+    instance.profile.save()
 
 class Park(models.Model):
     name = models.CharField(max_length=100)
